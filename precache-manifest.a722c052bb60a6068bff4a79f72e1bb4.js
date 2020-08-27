a46de3ea1f1ee548167cb3905c0df42bbd0e6b01
Raunak Kedia <raunakkedia1@gmail.com>
2020-08-28 00:54:52 +0530

    Updates

---
+++ precache-manifest.a722c052bb60a6068bff4a79f72e1bb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6898348d5aedcc7ce67fa365dbb7702",
+    "url": "/news-ai/index.html"
+  },
+  {
+    "revision": "142617ce180be8c299ff",
+    "url": "/news-ai/static/css/main.378bc379.chunk.css"
+  },
+  {
+    "revision": "acefab654b99db746d1a",
+    "url": "/news-ai/static/js/2.d40c3a5c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/news-ai/static/js/2.d40c3a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142617ce180be8c299ff",
+    "url": "/news-ai/static/js/main.85e84cf0.chunk.js"
+  },
+  {
+    "revision": "c385b5befa72913b5ef2",
+    "url": "/news-ai/static/js/runtime-main.981f4bd4.js"
+  }
+]);
